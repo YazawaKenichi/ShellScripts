5e96486058ea6d604754384a867935e16015f98b
YazawaKenichi <tonfarken83825@gmail.com>
2021-10-21 04:11:31 +0900

    何が変更されてんだかよくわからんけど一応コミット

--- pico/picobuild.sh
+++ pico/picobuild.sh
@@ -3,9 +3,9 @@
 echo -n ProjectPass(Relative):
 read STR
 
+cp ./pico-sdk/external/pico_sdk_import.cmake ./STR
 mkdir ./STR/build
 cd ./STR/build
-export PICO_SDK_PATH=../../pico-sdk
+export PICO_SDK_PATH=./pico-sdk
 cmake ..
-cd ../
 make -j4
