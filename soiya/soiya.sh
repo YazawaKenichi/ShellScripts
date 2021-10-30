c0c8165baa71c7ca98aa312532e4dba5e4ad7fbb
YazawaKenichi <tonfarken83825@gmail.com>
2021-10-30 16:24:35 +0900

    ソイヤコマンド追加

---
+++ soiya/soiya.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+while true
+do
+	echo -n -e "\r\033[31m\033[1m SOIYA! L(‘ω’)┘ "
+	sleep 0.5
+	echo -n -e "\r\033[34m\033[1m SOIYA! └(‘ω’)」"
+	sleep 0.5
+done
+echo -n -e "\033[0m"
+
+# https://qiita.com/blueskyarea/items/f07466393ccb5efb3dc5	// sleep コマンド
+# https://www.mm2d.net/main/prog/c/console-02.html	// ANSI エスケープコード
+# https://ktpcschool.com/imageToAscii/	// アスキーアート作成
+
